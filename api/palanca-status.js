dd82996b78cc01fe3ecf7a1118acf42fb11c2c32
noul <148019647+Noulll@users.noreply.github.com>
2025-12-31 20:34:19 -0500

    Add CORS headers to palanca-status handler

--- api/palanca-status.js
+++ api/palanca-status.js
@@ -6,6 +6,9 @@ const redis = new Redis({
 });
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method !== "GET") return res.status(405).end();
 
   let palancas = await redis.get("palancasData");
