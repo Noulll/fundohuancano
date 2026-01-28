f5b3fb27fa0b56599b83627e822c1fa542749a66
noul <148019647+Noulll@users.noreply.github.com>
2026-01-27 22:39:02 -0500

    Add CORS headers to status handler

--- api/status.js
+++ api/status.js
@@ -1,3 +1,6 @@
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   return res.status(200).json({ message: "OK" });
 }
